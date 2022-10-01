a1f450eb10c62644fa5fcca005b2b06f679fa5a0
Jean Pierre <jeanjara.15926@gmail.com>
2022-09-30 21:39:56 -0500

    1

--- dal/consulta.js
+++ dal/consulta.js
@@ -6,10 +6,11 @@ let pool = cnx.pool;
 
 
 const consulta = (request, response) => {
-    console.log("consulta");
+    ruc = request.body.ruc;
     let cadena = 'select * from cliente \n\r' +
-     'where ruc = \'20388021196\'';
-    pool.query('select * from name_table',(error, results)=>{
+                'where ruc = \' ' + ruc +' \'';
+    pool.query('select * from name_table',[request.body.ruc],
+    (error, results)=>{
         if (error) {
             response.status(200).json({ estado: false, mensaje: "error: en la consulta BD", data: null });
         } else {
